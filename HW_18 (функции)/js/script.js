5b0c1c222121d5bcffc3cde094267d85b98e1feb
Andrey Gurenko <89606947+AN-LITE@users.noreply.github.com>
2022-04-13 16:51:56 +0300

    HW_18

---
+++ HW_18 (функции)/js/script.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const arr = [1, 2, 3, -1, -2, -3];
+
+function even (arr){
+
+    let evenArr= []
+
+    if (evenArr.length === 0 ){
+        return 'Массив пустой'
+    }
+
+    for (let i = 0; i < arr.length ;i++){
+        
+        if ((arr[i] % 2) === 0){
+
+            evenArr.push(currentPositiveNumber);
+
+        }
+
+        return console.log(evenArr);
+  	    
+    }
+
+    
+}
+
